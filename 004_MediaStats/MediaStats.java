bd7c9945c16a597fb206f799c3285371d753226d
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-09-25 21:00:19 -0500

    004_Media Stats

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,30 @@
+public class MediaStats{
+    public static void main(String[] args){
+        int year=1847;
+        int page=300;
+        int Runtime=132;
+
+        System.out.println("The Jane Eyre was released in "+ year +" and is "+ page + " pages long.");
+   
+        year=1960;
+        page=218;
+        System.out.println("The To Kill a Mockingbrid  was released in "+ year +" and is "+ page + " pages long.");
+   
+        year=1992;
+        page=326;
+        System.out.println("The To Live  was released in "+ year +" and is "+ page + " pages long.");
+   
+        year=2012;
+        Runtime=110;
+        System.out.println("The To Live  was released in "+ year +" and is "+ Runtime + " min long.");
+
+        year=2001;
+        Runtime=121;
+        System.out.println("The Jane Eyre  was released in "+ year +" and is "+ Runtime + " min long.");
+   
+        year=2009;
+        Runtime=162;
+        System.out.println("The Avatar  was released in "+ year +" and is "+ Runtime + " min long.");
+    }
+}
+        
