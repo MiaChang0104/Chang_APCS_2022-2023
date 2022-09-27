416d60a126335ece3a418e7232a6fc43bdbc9200
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-09-27 00:29:40 -0500

    work

---
+++ 006_TemperatureFlux/TemperatureFlux.java
@@ -0,0 +1,9 @@
+public class TemperatureFlux {
+    public static void main(String [] agrs){
+    int high=17;
+    int low=6;
+    int diff=high-low;
+    //add ;
+    System.out.println("Today's High:"+high+" Today's low:"+low+" Today's difference:"+diff);
+    }
+}
---
+++ 008_RefactorATableOfVaria/RefactorATableVariables.java
@@ -0,0 +1,15 @@
+public class RefactorATableVariables {
+    public static void main(String[] args) {
+        int a=1;
+        for (int i = 1;i<=4;i++){
+        int b=a*a;
+        System.out.println(a+"*"+a+"="+b);
+        //a^2
+        b=a*a*a;
+        System.out.println(a+"*"+a+"*"+a+"="+b);
+        //a^3
+        a=a+1;
+        }
+    }
+
+}
