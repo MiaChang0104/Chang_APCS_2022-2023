bd7ead2096bb0c32594c7bb7d1c0ef8130950e0c
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-09-27 09:35:17 -0500

    004

---
+++ 004_MediaStats/MediaStats.java
@@ -0,0 +1,33 @@
+public class MediaStats {
+    public static void main(String[] args){
+        int year= 1847;
+        int page= 210;
+        int runtime= 100;
+
+        System.out.println("The Jane Eyre was released in " + year +" and is "+page +" pages long.");
+
+        year=1992;
+        page=300;
+
+        System.out.println("The Life was released in " + year +" and is "+page +" pages long.");
+
+        year=1993;
+        page=400;
+        System.out.println("The The club was released in " + year +" and is "+page +" pages long.");
+
+        year=2003;
+        System.out.println("The Avatar  was released in " + year +" and is "+runtime +" min long.");
+
+        year=2015;
+        runtime=91;
+        System.out.println("The Minions  was released in " + year +" and is "+runtime +" min long.");
+
+        year=2001;
+        runtime=155;
+        System.out.println("The Harry Potter was released in " + year +" and is "+runtime +" min long.");
+
+
+
+        
+    }
+}
