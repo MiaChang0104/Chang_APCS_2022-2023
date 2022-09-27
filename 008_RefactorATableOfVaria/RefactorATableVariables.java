ba88de7463ec77a64f63b1da81e616b3857dfb2f
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-09-27 10:07:37 -0500

    009

--- 008_RefactorATableOfVaria/RefactorATableVariables.java
+++ 008_RefactorATableOfVaria/RefactorATableVariables.java
@@ -1,12 +1,13 @@
-public class RefactorATableVariables {
+public class RefactorATableOfVariables {
     public static void main(String[] args) {
+        System.out.println("a   a^2   a^3");
         int a = 1;
         for (int i = 1; i <= 4; i++) {
-            int b = a * a;
-            System.out.println(a + "*" + a + "=" + b);
+            int aSquared = a * a;
+            
             // a^2
-            b = a * a * a;
-            System.out.println(a + "*" + a + "*" + a + "=" + b);
+            int aCube = a * a * a;
+            System.out.println(a + "   " + aSquared + "     " + aCube);
             // a^3
             a = a + 1;
         }
