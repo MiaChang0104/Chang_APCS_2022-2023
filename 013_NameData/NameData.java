9633c15fe66b401ce458d02ed38ae826aa7732c6
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-10-13 10:20:19 -0500

    homework

--- 013_NameData/NameData.java
+++ 013_NameData/NameData.java
@@ -1,3 +1,13 @@
 public class NameData {
-    
+    public static void main(String[] args) {
+    NameTransmogrifier name = new NameTransmogrifier("Mia");
+    //making a new object
+    System.out.println(name.name);
+    System.out.println(name.nameReversed);
+    System.out.println(name.nameUpperCase);
+    System.out.println(name.nameLowerCase);
+    System.out.println(name.nameLength);
+    System.out.println(name.nameFirstLetter);
+    System.out.println(name.nameLastLetter);
+    }
 }
--- 014_SweetGarage/SweetGarage.java
+++ 014_SweetGarage/SweetGarage.java
@@ -1,3 +1,14 @@
 public class SweetGarage {
-    
+    public static void main(String[] args) {
+        Car car1 = new Car(2011,201,"M","4",true );
+        System.out.println(car1.year);
+        System.out.println(car1.milesDriven);
+        System.out.println(car1.manufacturerName);
+        System.out.println(car1.modelName);
+        Car car2 = new Car(2012,200,"A","4",true );
+        System.out.println(car1.year);
+        System.out.println(car1.milesDriven);
+        System.out.println(car1.manufacturerName);
+        System.out.println(car1.modelName);
+    }
 }
