689c5df5873dcda4614491f127417ae3291da5dd
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-10-18 10:20:17 -0500

    homework

--- 015_TurtleBoxer/TurtleBoxer.java
+++ 015_TurtleBoxer/TurtleBoxer.java
@@ -1,5 +1,11 @@
-package 015_TurtleBoxer;
+import javax.swing.*;
+import javax.swing.text.WrappedPlainView;
+
+import java.awt.event.*;
+import java.util.*;
+import java.awt.*;
 
 public class TurtleBoxer {
+
     
 }
--- 016_CarAge/ReturningCalculator.java
+++ 016_CarAge/ReturningCalculator.java
@@ -1,5 +1,43 @@
-package 016_CarAge;
+public class ReturningCalculator
+{
+    public static void main(String[] args)
+    {
 
-public class ReturningCalculator {
+    }
+
+    public int integerAdder(int x, int y)
+    {
+        return x+y;
+    }
+
+    public double doubleAdder(double x, double y)
+    {
+        return x+y;
+    }
     
+    public int integerSubtractor(int x, int y)
+    {
+        return x-y;
+    }
+
+    public double doubleSubtractor(double x, double y)
+    {
+        return x-y;
+    }
+
+    
+    public int integerMultiplier(int x, int y)
+    {
+        return x*y;
+    }
+
+    public double doubleMultiplier(double x, double y)
+    {
+        return x*y;
+    }
+
+    public double divider(double x, double y)
+    {
+        return x/y;
+    }
 }
