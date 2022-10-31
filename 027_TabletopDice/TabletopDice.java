a5b63c5fdb9993ee5e056b8916a2adb783a51286
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-10-31 10:06:57 -0500

    027

---
+++ 027_TabletopDice/TabletopDice.java
@@ -0,0 +1,17 @@
+public class TabletopDice {
+    public static void main(String[] args) {
+        int d4 = (int) (Math.random() * 4 + 1);
+        System.out.println("4 side dice " + d4);
+        int d6 = (int) (Math.random()*6+1);
+        System.out.println("6 side dice " + d6);
+        int d8 = (int) (Math.random() * 8 + 1);
+        System.out.println("8 side dice " + d8);
+        int d10 = (int) (Math.random() * 10 + 1);
+        System.out.println("4 side dice " + d10);
+        int d12 = (int) (Math.random() * 12 + 1);
+        System.out.println("4 side dice " + d12);
+        int d20 = (int) (Math.random() * 20 + 1);
+        System.out.println("4 side dice " + d20);
+
+    }
+}
