19b612421a1d199418edb616a561016b370b9300
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-11-04 09:15:20 -0500

    homework

---
+++ 032_ShippingCalculator/ShippingCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class ShippingCalculator {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+        System.out.println("Type your weight of package here : ");
+        double number = scan.nextDouble();
+        if (number > 10 && number <= 20) {
+            System.out.println(number + "pounds need 10.5 dollars");
+        } else if (number>3 && number<=10){
+            System.out.println(number + "pounds need 8.5 dollars");
+        } else if (number>1 && number<=3){
+            System.out.println(number + "pounds need 5.5 dollars");
+        } else if (number>3.5 && number<=1){
+            System.out.println(number + "pounds need 3.5 dollars");
+        } else if (number<=0) {
+            System.out.println("Invalid input");
+        } else {
+            System.out.println("This package cannot be shipped");
+        }
+    }
+}
