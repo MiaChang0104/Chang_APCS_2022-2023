063ada3c98062b681fc7f7fe6a4b5d0abb5f1078
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-11-08 10:20:21 -0600

    homework

--- 018_LorumlpsumStringMethods/LorumlpsumStringMethods.java
+++ 018_LorumlpsumStringMethods/LorumlpsumStringMethods.java
@@ -1,3 +1,10 @@
-public class LorumlpsumStringMethods{
-     
+public class LorumlpsumStringMethods {
+    public static void main(String[] args) {
+        String world = "Lorem ipsum dolor sit amet Dolor purus non enim praesent elementum facilisis leo. Odio ut enim blandit volutpat maecenas. Nibh mauris cursus mattis molestie. Ornare suspendisse sed nisi lacus sed viverra.";
+        System.out.println(world.length());
+        String first = "Lorem ipsum dolor sit amet Dolor purus non enim praesent elementum facilisis leo. ";
+        String first2= first.toUpperCase();
+        System.out.println(first.length());
+
+    }
 }
