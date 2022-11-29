78a355843a124ca3910c747b544986945b4bb122
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-11-29 09:14:56 -0600

    039

--- 038_SimpleCounter/SimpleCounter.java
+++ 038_SimpleCounter/SimpleCounter.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
+
 public class SimpleCounter {
     public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Type one number here: ");
-    int num = scan.nextInt();
-    int i =0;
-    while(i<num){
-        i++;
-        System.out.println(i);
-    }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type one number here: ");
+        int num = scan.nextInt();
+        int i = 0;
+        while (i < num) {
+            i++;
+            System.out.println(i);
+        }
 
     }
 }
+//i++;
--- 039_PowerAttack/PowerAttack.java
+++ 039_PowerAttack/PowerAttack.java
@@ -1,15 +1,16 @@
 public class PowerAttack {
     public static void main(String[] args) {
-    int number = (int) (Math.random()*10+1);
-    int character = number+3;
-    int TotalMonster=11 +13;
-    int weapon = (int) (Math.random()*6+4);
-    int TotalAttack = character+weapon;
-    while (TotalAttack<TotalMonster){
-        TotalMonster-=TotalAttack;
-        System.out.println("First term Mondter only have " + TotalMonster);
-    }
-
-    
+        int number = (int) (Math.random() * 10 + 1);
+        int number1 = (int) (Math.random() * 10 + 1);
+        int character = number + 3;
+        int character1 = number1 + 3;
+        int totalMonster = 11 + 13;
+        int weapon = (int) (Math.random() * 6 + 4);
+        int weapon1 = (int) (Math.random() * 6 + 4);
+        int totalAttack = character + weapon;
+        int totalAttack1 = character1 + weapon1;
+        System.out.println("First attack is " + character);
+        System.out.println("Second attack is " + character1);
+        
     }
 }
