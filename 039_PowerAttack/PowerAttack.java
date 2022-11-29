57bf9e47329060b89d89d8f2695ebe3ef5792246
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-11-29 14:45:07 -0600

    039

--- 039_PowerAttack/PowerAttack.java
+++ 039_PowerAttack/PowerAttack.java
@@ -1,16 +1,16 @@
 public class PowerAttack {
     public static void main(String[] args) {
-        int number = (int) (Math.random() * 10 + 1);
-        int number1 = (int) (Math.random() * 10 + 1);
-        int character = number + 3;
-        int character1 = number1 + 3;
-        int totalMonster = 11 + 13;
-        int weapon = (int) (Math.random() * 6 + 4);
-        int weapon1 = (int) (Math.random() * 6 + 4);
-        int totalAttack = character + weapon;
-        int totalAttack1 = character1 + weapon1;
-        System.out.println("First attack is " + character);
-        System.out.println("Second attack is " + character1);
-        
+        //character
+        int strength = 3;
+        int attack = (int)(Math.random()*10+1);
+        int weapon = 4+(int)(Math.random()*3);
+        int characterTotal = strength + attack;
+        //monster
+        int armor = 11;
+        int monster=13;
+        while (characterTotal>=armor){
+            monster-=weapon;
+            System.out.println("Monster only have" +monster );
+        }
     }
 }
