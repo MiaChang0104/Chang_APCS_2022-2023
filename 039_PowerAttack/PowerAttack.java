230612f5efa904b277a75babeb7227a4670e8a02
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-12-01 10:20:14 -0600

    041

--- 039_PowerAttack/PowerAttack.java
+++ 039_PowerAttack/PowerAttack.java
@@ -2,15 +2,20 @@ public class PowerAttack {
     public static void main(String[] args) {
         //character
         int strength = 3;
-        int attack = (int)(Math.random()*10+1);
-        int weapon = 4+(int)(Math.random()*3);
+        int attack = (int) (Math.random() * 10 + 1);
+        int weapon = 4 + (int) (Math.random() * 3);
         int characterTotal = strength + attack;
         //monster
         int armor = 11;
-        int monster=13;
-        while (characterTotal>=armor){
-            monster-=weapon;
-            System.out.println("Monster only have" +monster );
+        int monster = 13;
+        if (characterTotal < armor){
+            System.out.println("Sorry your only got " + characterTotal);
+        } else {
+            while((monster - weapon) >= 0) {
+                monster -= weapon;
+            }
+            System.out.println("Monster only got " + monster);
         }
+        
     }
 }
---
+++ 041_FizzBuzz/FizzBuzzForLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FizzBuzzForLoop {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type your number here: ");
+        int number = scan.nextInt();
+        int i = 0;
+        for (i = 0; i <= number; i++) {
+            if (i % 3 ==0 && i % 5 == 0 ) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }// i = 0;
+    }
+}
