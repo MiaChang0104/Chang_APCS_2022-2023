9cee5193427ca1a611e12b5d9c2f7052c817a9ca
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2022-12-02 09:15:13 -0600

    043

--- 042_DivisiblelnRange/DivisiblelnRange.java
+++ 042_DivisiblelnRange/DivisiblelnRange.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
+
 public class DivisiblelnRange {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        System.out.println("Type your first number here:");
+        int number1 = scan.nextInt();
+        System.out.println("Type your second number here:");
+        int number2 = scan.nextInt();
+        System.out.println("Type your third number here:");
+        int number3 = scan.nextInt();
+        for (int i = number1; i <= number2; i++) {
+            if (i % number3 == 0) {
+                System.out.println(i);
+
+            } else {
+                System.out.println(" ");
+            }
+        }
     }
 }
---
+++ 043_LoopYourName/LoopYourName.java
@@ -0,0 +1,8 @@
+public class LoopYourName {
+    public static void main(String[] args) {
+        String name = "Mia Chang"
+        for (i = 0; i < name.substring(8); i++) {
+            system.out
+        }
+    }
+}
