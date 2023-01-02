0a775dcba67bd8506d92028718d0847f4f3fb064
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2023-01-02 11:29:38 -0600

    045

--- 045_BasicNestedPractice/BasicNestedPractice.java
+++ 045_BasicNestedPractice/BasicNestedPractice.java
@@ -1,20 +1,10 @@
 import java.util.Scanner;
 
-public class BasicNestedPractice {
+public class BasicNestedPractice{
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Type your number here:");
-        String number = scan.nextLine();
-        for (int i = 0; i < number.length(); i++) {
-            for (int j = 0; j < number.length(); j++) {
-
-                if (number.length() <= 20) {
-                    System.out.print(i + "-" + j + "\t");
-                } else {
-                    System.out.println("Can't be bigger than 20");
-                    break;
-                }
-            }
-        }
+        Scanner scan  new Scanner(System.in);
+        System.out.println("Type in here");
+        String word = scan.nextLine();
+        
     }
 }
