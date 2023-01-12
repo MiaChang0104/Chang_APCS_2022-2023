2380692b375fda459ea0195af3b231ca8cc4d2f6
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2023-01-12 10:19:27 -0600

    051

--- 050_NameGreeting/NameGreeting.java
+++ 050_NameGreeting/NameGreeting.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+
 public class NameGreeting {
     public static void main(String[] args) {
-        hi();
-    }
-    public static void hi(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Type your name here:");
-        
+        String name = scan.nextLine();
+        method();
+    }
+// make sure it's same
+    public static void method () {
+
+        System.out.println("Hello,");
+
     }
 }
