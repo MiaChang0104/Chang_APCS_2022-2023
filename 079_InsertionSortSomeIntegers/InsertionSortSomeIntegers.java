097f511600fa2458dfa10f325d443f0e75b553a9
MiaChang0104 <113122300+MiaChang0104@users.noreply.github.com>
2023-05-08 09:14:28 -0500

    079

--- 079_InsertionSortSomeIntegers/InsertionSortSomeIntegers.java
+++ 079_InsertionSortSomeIntegers/InsertionSortSomeIntegers.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
 import java.util.Random;
-
 public class InsertionSortSomeIntegers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("How many random integers do you need ?");
         int number = scan.nextInt();
         Random random = new Random();
-        int[] arr = new int[number];
-        for (int i = 0; i < number; i++) {
-            arr[i] = random.nextInt(1000);
-            System.out.print(arr[i]+" ");
+        int[] num = new int[number];
+        for (int i = 0; i < number; i++){
+            num[i] = random.nextInt(1000);
+            
+            System.out.println(num[i]);
         }
-    public static int[] insertionSorter(int[] arr) {
-            for(int i = 1; i < arr.length; i++) {
-                int temp = arr[i];
-                int possibleIndex = i;
-    
-                while(possibleIndex > 0 && temp < arr[possibleIndex - 1]){
-                    arr[possibleIndex] = arr[possibleIndex - 1];
-                    possibleIndex--;
-                }
-    
-                arr[possibleIndex] = temp;
+        
+    }
+
+    public static int[] insertionSorter(int[] num) {
+        for (int i = 1; i < num.length; i++) {
+            int temp = num[i];
+            int possibleIndex = i;
+
+            while (possibleIndex > 0 && temp < num[possibleIndex - 1]) {
+                num[possibleIndex] = num[possibleIndex - 1];
+                possibleIndex--;
             }
-    
-            return arr;   
-        } 
-       
-    
+            num[possibleIndex] = temp;
+        }
+        System.out.println(num);
+
+        return num;
+    }
 }
